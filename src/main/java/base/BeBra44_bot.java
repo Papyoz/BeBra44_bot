735a00acd949bfb98798d422fdb65d8691ef5fd0
Papyoz <niknatbs28@list.ru>
2022-08-28 13:40:20 +0300

    java: unreported exception org.telegram.telegrambots.meta.exceptions.TelegramApiException; must be caught or declared to be thrown

--- src/main/java/base/BeBra44_bot.java
+++ src/main/java/base/BeBra44_bot.java
@@ -1,2 +1,25 @@
-package base;public class BeBra44_bot {
+package base;
+
+import org.telegram.telegrambots.bots.DefaultAbsSender;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public class BeBra44_bot extends DefaultAbsSender {
+
+    public BeBra44_bot(DefaultBotOptions options){
+        super(options);
+    }
+    @Override
+    public String getBotToken(){
+        return "5472971106:AAEQid78UH5Z_x2jbF3g6wzfqy_ibC98xHc";
+    }
+    public String getBotURL(){
+        return "t.me/BeBra4_bot";
+    }
+
+    public static void main(String[] args){
+        BeBra44_bot bot = new BeBra44_bot(new DefaultBotOptions());
+        bot.execute(SendMessage.builder().chatId("5144539738").text("Ku").build());
+    }
 }
+//"5144539738 ku"
