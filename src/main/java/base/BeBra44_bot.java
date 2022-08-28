ebea6466c8556bfc927f6f17390c4da89a6fdfe2
Papyoz <niknatbs28@list.ru>
2022-08-28 17:41:02 +0300

    Версия с GitHub(Рабочая)

--- src/main/java/base/BeBra44_bot.java
+++ src/main/java/base/BeBra44_bot.java
@@ -1,5 +1,6 @@
 package base;
 
+
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -12,26 +13,18 @@ import org.telegram.telegrambots.bots.DefaultAbsSender;
 import java.util.logging.Level;
 
 public class BeBra44_bot extends TelegramLongPollingBot {
-    public BeBra44_bot(DefaultBotOptions options) {
-        super(options);
-    }
 
-    @Override
-    public void onUpdateReceived (Update update) {
-    }
     @Override
     public String getBotUsername() {
         return "Bebra4_bot";
     }
-
     @Override
     public String getBotToken() {
         return "5472971106:AAEQid78UH5Z_x2jbF3g6wzfqy_ibC98xHc";
     }
 
 
-    public void BOT (Update update) {
-        // We check if the update has a message and the message has text
+    public void onUpdateReceived (Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
             message.setChatId(update.getMessage().getChatId().toString());
