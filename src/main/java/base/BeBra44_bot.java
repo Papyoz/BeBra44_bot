21261efaadd633fec22d2d33553c9040d7269c3e
Papyoz <niknatbs28@list.ru>
2022-08-28 17:21:40 +0300

    Версия с GitHub

--- src/main/java/base/BeBra44_bot.java
+++ src/main/java/base/BeBra44_bot.java
@@ -1,25 +1,47 @@
 package base;
 
-import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.bots.DefaultAbsSender;
 
-public class BeBra44_bot extends DefaultAbsSender {
+import java.util.logging.Level;
 
-    public BeBra44_bot(DefaultBotOptions options){
+public class BeBra44_bot extends TelegramLongPollingBot {
+    public BeBra44_bot(DefaultBotOptions options) {
         super(options);
     }
+
     @Override
-    public String getBotToken(){
-        return "5472971106:AAEQid78UH5Z_x2jbF3g6wzfqy_ibC98xHc";
+    public void onUpdateReceived (Update update) {
     }
-    public String getBotURL(){
-        return "t.me/BeBra4_bot";
+    @Override
+    public String getBotUsername() {
+        return "Bebra4_bot";
     }
 
-    public static void main(String[] args){
-        BeBra44_bot bot = new BeBra44_bot(new DefaultBotOptions());
-        bot.execute(SendMessage.builder().chatId("5144539738").text("Ku").build());
+    @Override
+    public String getBotToken() {
+        return "5472971106:AAEQid78UH5Z_x2jbF3g6wzfqy_ibC98xHc";
+    }
+
+
+    public void BOT (Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
-//"5144539738 ku"
---
+++ src/main/java/base/Main.java
@@ -0,0 +1,18 @@
+package base;
+
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new BeBra44_bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
